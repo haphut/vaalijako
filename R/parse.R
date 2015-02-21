afd6645a424608e91bb690a2a767df1e034a13d2
Petri Kola <petri.kola@gmail.com>
2015-02-21 14:49:46 +0200

    Function load_and_cache_csv for loading data

--- R/parse.R
+++ R/parse.R
@@ -7,3 +7,8 @@ parse_csv <- function(filename) {
     # FIXME: There are duplicated building_ids. Are all of the properties identical for those rows?
 }
 
+library(httr)
+load_and_cache_csv <- function () {
+  url <- "https://www.avoindata.fi/data/fi/dataset/df2d00d2-4a00-46ba-b16c-8f73c5e6bb32/resource/b9e48d8e-f8c3-4dde-b221-45e495021c9c/download/rvaalit2015eduskuntavaalitdataavoindatarvaalit2015eduskuntavaalitdataavoindata1003796rakaanestysalu"
+  GET(url, write_disk("inst/voting-areas.csv", overwrite = TRUE))
+}
--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ```R
 library(devtools) # if missing, run install.packages('devtools')
 install_github('haphut/vaalijako')
+install.packages("httr")
 ```
 
 The data is available from [avoindata.fi](https://www.avoindata.fi/data/fi/dataset/rakennusten-aanestysalueet).
