bcf319906364ea6db3bf09d737746e8b42ef4f30
haphut <haphut@gmail.com>
2015-03-06 23:32:17 +0200

    Change factors to characters.

--- R/parse.R
+++ R/parse.R
@@ -5,7 +5,7 @@ parse_voting_area_csv <- function(filename) {
   df <- read.csv(filename, sep = ';', header = FALSE,
                  col.names = c('building_id', 'northing', 'easting',
                                'municipality', 'voting_area', 'NULL'),
-                 colClasses = c('factor', 'integer', 'integer', 'factor',
-                                'factor', 'NULL'))
+                 colClasses = c('character', 'integer', 'integer', 'character',
+                                'character', 'NULL'))
 }
 
